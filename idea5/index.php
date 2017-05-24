951a0bd97e23d10cdb2c4635a9317b4687bf0089
Alex Westphal <awestphal@onpointlearning.com>
2017-05-24 12:53:00 +1200

    Added basic idea5

---
+++ idea5/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en-nz">
+<head>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+  <title>EuroMoot</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css"/>
+  <link rel="stylesheet" href="/idea3/font-awesome-4.7.0/css/font-awesome.css"/>
+
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+  <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+
+  <style type="text/css">
+    @font-face {
+      font-family: "Kingthings Foundation";
+      src: url('Kingthings_Foundation.ttf');
+    }
+
+    body {
+      background-color: #039;
+    }
+
+    #container {
+      margin: 0 auto;
+      padding: 10px 0;
+    }
+
+    a.linkbox {
+      display: block;
+      padding: 5px 10px;
+      margin-bottom: 10px;
+      background-color: #fc0;
+      color: #039;
+
+      border-radius: 5px;
+
+      font-size: 28px;
+      font-family: "Kingthings Foundation";
+    }
+
+    .linkbox > i.fa {
+      float: right;
+      padding: 7px;
+    }
+
+    #content {
+      background-color: white;
+      min-height: 1104px;
+
+      border-radius: 5px;
+      padding: 5px 10px;
+    }
+  </style>
+</head>
+<body>
+  <div id="container" class="row">
+    <div class="column large-4">
+      <img src="/logos/euro-logo6-kingthings.png"/>
+      <a class="linkbox">News <i class="fa fa-newspaper-o"></i></a>
+      <a class="linkbox">What? <i class="fa fa-question"></i></a>
+      <a class="linkbox">When? <i class="fa fa-clock-o"></i></a>
+      <a class="linkbox">Where? <i class="fa fa-map-marker"></i></a>
+      <a class="linkbox">Themes <i class="fa fa-empire"/></i></a>
+      <a class="linkbox">Tours <i class="fa fa-map-signs"/></i></a>
+      <a class="linkbox">Merchandise <i class="fa fa-shopping-cart"/></i></a>
+      <a class="linkbox">Trophies <i class="fa fa-trophy"/></i></a>
+      <a class="linkbox">MOC <i class="fa fa-users"/></i></a>
+      <a class="linkbox">Contact Us <i class="fa fa-at"/></i></a>
+      <a class="linkbox">Facebook <i class="fa fa-facebook"/></i></a>
+      <a class="linkbox">Instagram <i class="fa fa-instagram"/></i></a>
+    </div>
+    <div id="content" class="column large-8" ></div>
+  </div>
+</body>
+</html>
