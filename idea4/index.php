2d146dd89be5554993f529b9946b5b59a0bec08a
Alex Westphal <awestphal@onpointlearning.com>
2017-05-25 17:50:13 +1200

    Implemented idea4 box expand

--- idea4/index.php
+++ idea4/index.php
@@ -19,73 +19,129 @@
   </div>
   <div class="box-set">
     <div class="box preview">
-      <div class="box-title">News <i class="fa fa-newspaper-o"></i></div>
+      <div class="box-title">News
+        <span class="icon box-icon"><i class="fa fa-newspaper-o"></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+      </div>
       <div class="box-preview">
         Latest news and updates from the MOC.
       </div>
+      <div class="box-full">News</div>
     </div>
     <div class="box preview">
-      <div class="box-title">What? <i class="fa fa-question"/></i></div>
+      <div class="box-title">What?
+        <span class="icon box-icon"><i class="fa fa-question"/></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+        </div>
       <div class="box-preview">
         A 'moot' is a gathering of Rovers. It is the premier event on the Rover calendar.
         An action packed long weekend, filled to the brim with fun, friends and adventure.
       </div>
+      <div class="box-full">What?</div>
     </div>
     <div class="box preview">
-      <div class="box-title">When? <i class="fa fa-clock-o"/></i></div>
+      <div class="box-title">When?
+        <span class="icon box-icon"><i class="fa fa-clock-o"/></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+      </div>
       <div class="box-preview">
         <span class="euromoot">EUROMOOT</span> will run from 29 March to 2 April 2018.
       </div>
+      <div class="box-full">When</div>
     </div>
     <div class="box preview">
-      <div class="box-title">Where? <i class="fa fa-map-marker"/></i></div>
+      <div class="box-title">Where?
+        <span class="icon box-icon"><i class="fa fa-map-marker"/></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+      </div>
       <div class="box-preview">
         <span class="euromoot">EUROMOOT</span> will be held at Mania-O-Roto (MOR) Scout Park,
         Chalmers Ave, Ashburton, Canterbury, New Zealand.
       </div>
+      <div class="box-full">Where?</div>
     </div>
     <div class="box preview">
-      <div class="box-title">Themes <i class="fa fa-empire"/></i></div>
+      <div class="box-title">Themes
+        <span class="icon box-icon"><i class="fa fa-empire"/></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+      </div>
       <div class="box-preview">
         Each night at Moot we have a party with a fun theme! Dress up with your crew and
         be as creative as possible! Find out what this years themes are here.
       </div>
+      <div class="box-full">Themes</div>
     </div>
     <div class="box preview">
-      <div class="box-title">Tours <i class="fa fa-map-signs"/></i></div>
+      <div class="box-title">Tours
+        <span class="icon box-icon"><i class="fa fa-map-signs"/></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+      </div>
       <div class="box-preview">
           Check out our range of amazing tours!
       </div>
+      <div class="box-full">Tours</div>
     </div>
     <div class="box preview">
-      <div class="box-title">Merchandise <i class="fa fa-shopping-cart"/></i></div>
+      <div class="box-title">Merchandise
+        <span class="icon box-icon"><i class="fa fa-shopping-cart"/></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+      </div>
       <div class="box-preview">
         Browse our selection of exclusive moot merchandise.
       </div>
+      <div class="box-full">Merchandise</div>
     </div>
     <div class="box preview">
-      <div class="box-title">Trophies <i class="fa fa-trophy"/></i></div>
+      <div class="box-title">Trophies
+        <span class="icon box-icon"><i class="fa fa-trophy"/></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+      </div>
       <div class="box-preview">
         A number of trophies are up for grabs.
       </div>
+      <div class="box-full">Trophies</div>
     </div>
     <div class="box preview">
-      <div class="box-title">MOC <i class="fa fa-users"/></i></div>
+      <div class="box-title">MOC
+        <span class="icon box-icon"><i class="fa fa-users"/></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+      </div>
       <div class="box-preview">
         Come meet our wonderful Moot Organising Committee (MOC).
       </div>
+      <div class="box-full">MOC</div>
     </div>
     <div class="box preview">
-      <div class="box-title">Contact Us <i class="fa fa-at"/></i></div>
+      <div class="box-title">Contact Us
+        <span class="icon box-icon"><i class="fa fa-at"/></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+      </div>
       <div class="box-preview">
         If you need to contact us for any reason.
       </div>
+      <div class="box-full">Contact Us</div>
+    </div>
+    <div class="box preview">
+      <div class="box-title">Search
+        <span class="icon box-icon"><i class="fa fa-search"/></i></span>
+        <a class="icon close" title="Close Box"><i class="fa fa-times"></i></a>
+      </div>
+      <div class="box-preview">
+        Find the information you need.
+      </div>
+      <div class="box-full">Search</div>
     </div>
   </div>
   <div class="box-set">
-    <a class="linkbox" href="https://www.facebook.com/EuroMoot2018/" target="_blank" title="EuroMoot on Facebook">Facebook <i class="fa fa-facebook"></i></a>
-    <a class="linkbox" title="EuroMoot on Snapchat">Snapchat <i class="fa fa-snapchat"></i></a>
-    <a class="linkbox" href="https://www.instagram.com/EuroMoot2018/" target="_blank" title="EuroMoot on Instagram">Instagram <i class="fa fa-instagram"></i></a>
+    <a class="linkbox" href="https://www.facebook.com/EuroMoot2018/" target="_blank" title="EuroMoot on Facebook">
+      Facebook <span class="icon"><i class="fa fa-facebook"></i></span>
+    </a>
+    <a class="linkbox" title="EuroMoot on Snapchat">
+      Snapchat <span class="icon"><i class="fa fa-snapchat"></i></span>
+    </a>
+    <a class="linkbox" href="https://www.instagram.com/EuroMoot2018/" target="_blank" title="EuroMoot on Instagram">
+      Instagram <span class="icon"><i class="fa fa-instagram"></i></span>
+    </a>
   </div>
   <div class="scout-bar">
     <a href="http://www.scouts.org.nz/" target="_blank">
@@ -96,13 +152,49 @@
   <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
   <script>
     $(function() {
-      $(".box").click(function() {
-        var $box = $(this);
+      var boxColumns = 1;
+      var boxes = $(".box").toArray();
 
-        if($box.hasClass("preview")) {
-          // Only expand if showing preview
+
+      function openBox(box, index) {
+        boxes.forEach(closeBox);
+        $(box).removeClass("preview");
+
+        var newIndex = index - (index % boxColumns);
+        $(boxes[newIndex]).before(box);
+      }
+
+      function closeBox(box, index) {
+        var $box = $(box);
+        if(!$box.hasClass("preview")) {
+          if(index > 0) $(boxes[index-1]).after(box);
+          $box.addClass("preview");
         }
+      }
+
+      function checkWidth() {
+        var box0Y = $(".box:nth(0)").position().top;
+        var box1Y = $(".box:nth(1)").position().top;
+        var box2Y = $(".box:nth(2)").position().top;
+
+        boxColumns = box0Y == box2Y ? 3 : (box0Y == box1Y ? 2 : 1);
+      }
+
+      boxes.forEach(function(box, index) {
+        $(box).click(function() {
+          // If preview, open the box
+          if($(box).hasClass("preview")) openBox(box, index);
+        }).find(".box-title > a.icon.close").click(function(ev) {
+          ev.stopPropagation();
+          closeBox(box, index);
+        });
       });
+
+      $(window).resize(function() {
+        checkWidth();
+        boxes.forEach(closeBox);
+      });
+      checkWidth();
     });
   </script>
 </body>
