21f375fb528ecae3572c7c52b576ab62a9374cc8
Alex Westphal <awestphal@onpointlearning.com>
2017-05-25 15:34:09 +1200

    Improved responsive layout for idea4

--- idea4/index.php
+++ idea4/index.php
@@ -7,138 +7,11 @@
   <title>EuroMoot</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css"/>
   <link rel="stylesheet" href="/idea3/font-awesome-4.7.0/css/font-awesome.css"/>
+  <link rel="stylesheet" href="output.css"/>
 
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
   <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
 
-  <style type="text/css">
-    @font-face {
-      font-family: "Kingthings Foundation";
-      src: url('Kingthings_Foundation.ttf');
-    }
-
-    * {
-      box-sizing: border-box;
-    }
-
-    body {
-      margin: 0;
-      background-color: #039;
-      font-family: "Helvetica Neue";
-    }
-
-    .logo-container {
-      width: 400px;
-      margin: 20px auto;
-    }
-
-    .box-set {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: flex-start;
-    }
-
-    .box {
-      border-radius: 5px;
-      background-color: rgba(255,255,255, 0.8);
-    }
-
-    .linkbox {
-      border-radius: 5px;
-      padding: 5px 10px;
-      font-size: 28px;
-      font-family: "Kingthings Foundation";
-
-      background-color: #fc0;
-      color: #039;
-    }
-
-    .box > .box-title {
-      padding: 5px 10px;
-      font-size: 28px;
-      font-family: "Kingthings Foundation";
-
-      border-top-left-radius: 5px;
-      border-top-right-radius: 5px;
-      background-color: #fc0;
-      color: #039;
-    }
-
-    .box > .box-title > i.fa, .linkbox > i.fa {
-      float: right;
-      padding: 7px;
-    }
-
-    .box > .box-preview, .box > .box-full {
-      padding: 10px;
-      font-size: 20px;
-    }
-
-    .box:hover, .box:active {
-      cursor: pointer;
-    }
-
-    .box:hover > .box-title, .linkbox:hover {
-      color: steelblue;
-    }
-
-    .scout-bar {
-      height: 63px;
-      margin: 20px auto 0 auto;
-      background-color: #6a2c91;
-    }
-
-    .scout-bar img {
-      float: right;
-      width: 200px;
-    }
-
-    .euromoot { font-family: "Kingthings Foundation"; }
-
-
-    /* Response Sizing */
-
-    .box-set {
-      width: 1210px;
-      margin: 0 auto;
-      padding: 0 20px;
-    }
-
-    .box, .linkbox {
-      width: 350px;
-      margin: 20px;
-    }
-
-    .scout-bar {
-      width: 1130px;
-    }
-
-
-    @media screen and (max-width: 1210px) {
-      .box-set { width: 820px; }
-      .scout-bar { width: 740px; }
-    }
-
-    @media screen and (max-width: 820px) {
-      .box-set {
-        width: 100%;
-        margin: 0; padding: 0;
-      }
-
-      .box, .linkbox {
-        width: 100%;
-        margin: 10px 20px;
-      }
-
-      .scout-bar {
-        width: 100%;
-      }
-    }
-
-    @media screen and (max-width: 400px) {
-      .logo-container { width: 100%; margin: 0; }
-    }
-  </style>
 </head>
 <body>
   <div class="logo-container">
@@ -222,7 +95,15 @@
 
   <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
   <script>
+    $(function() {
+      $(".box").click(function() {
+        var $box = $(this);
 
+        if($box.hasClass("preview")) {
+          // Only expand if showing preview
+        }
+      });
+    });
   </script>
 </body>
 </html>
