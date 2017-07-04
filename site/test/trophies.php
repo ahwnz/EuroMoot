552e3166aa05718c006f87c602159ee343b5c3b2
Alex Westphal <awestphal@onpointlearning.com>
2017-07-04 16:29:46 +1200

    Added trophies

--- site/test/trophies.php
+++ site/test/trophies.php
@@ -5,6 +5,129 @@
 ?>
 <div id="trophies">
   <h2>Trophies</h2>
-  <p>A number of trophies are up for grabs.</p>
+  <p>The following are some of the trophies up for grabs at <span class="euromoot">EuroMoot</span>.</p>
+  <div class="grid-x grid-padding-x grid-padding-y">
+    <div class="small-12 medium-6 large-8 cell">
+      <h3>Zulu Shield</h3>
+      <p>
+        The most prestigious trophy in Rovers, awarded to the Rover Crew who
+        submits the most complete Log Book, which should show that the Crew has
+        maintained a balanced programme based on the aims of the Rover section.
+      </p>
+    </div>
+    <div class="small-12 medium-6 large-4 cell">
+      <img src="/trophies/trophy-zulu.png" alt="Zulu Shield"/>
+    </div>
+
+    <div class="small-12 medium-6 large-8 cell">
+      <h3>BP Lodge Service Shield</h3>
+      <p>
+        Previously awarded to Auckland crews by the local BP Lodge, this shield
+        is now awarded nationally to the crew who shows the most outstanding
+        service to Scouting and the community throughout the year leading up to Moot.
+      </p>
+    </div>
+    <div class="small-12 medium-6 large-4 cell">
+      <img src="/trophies/trophy-bp.png" alt="BP Lodge Service Shield"/>
+    </div>
+
+    <div class="small-12 medium-6 large-8 cell">
+      <h3>Helen Thomas Memorial Trophy</h3>
+      <p>
+        Awarded to a Rover, nominated by their Crew, as having shown outstanding
+        commitment to service within Scouting and the community, setting an
+        example to those around them, and who lives by the Scout Law. The
+        nominee may be a Full or Associate member who has not been in Rovers
+        more than two years.
+      </p>
+      <p>
+        Helen died tragically in a rafting accident while attending a National
+        Rover Moot.
+      </p>
+    </div>
+    <div class="small-12 medium-6 large-4 cell">
+      <img src="/trophies/trophy-helenthomas.png" alt="Helen Thomas Memorial Trophy"/>
+    </div>
+
+    <div class="small-12 medium-6 large-8 cell">
+      <h3>Fergus McLaren Banner</h3>
+      <p>
+        The other “big one.” Initially awarded for camping standards at Moot, but
+        now awarded to the Crew who contributes most to Moot spirit. The life of
+        Fergus McLaren, who was an Otago Rover killed in the Second World War,
+        is documented in the book “Fergus McLaren” which accompanies the banner.
+        This provides an understanding of the true meaning behind the banner.
+      </p>
+    </div>
+    <div class="small-12 medium-6 large-4 cell">
+      <img src="/trophies/trophy-fergus.png" alt="Fergus McLaren Banner"/>
+    </div>
+
+    <div class="small-12 medium-6 large-8 cell">
+      <h3>Nymph Trophy</h3>
+      <p>
+        Awarded to the Crew whose female Rovers contribute most to Moot Spirit.
+      </p>
+    </div>
+    <div class="small-12 medium-6 large-4 cell">
+      <img src="/trophies/trophy-nymph.png" alt="Nymph Trophy"/>
+    </div>
+
+    <div class="small-12 medium-6 large-8 cell">
+      <h3>Macho Trophy</h3>
+      <p>
+        Awarded to the Crew whose male Rovers contribute most to Moot Spirit.
+      </p>
+    </div>
+    <div class="small-12 medium-6 large-4 cell">
+      <img src="/trophies/trophy-macho.png" alt="Macho Trophy"/>
+    </div>
+
+    <div class="small-12 medium-6 large-8 cell">
+      <h3>Eastman Shield</h3>
+      <p>
+        Awarded to the “Fastest Rover at Moot”- the individual Rover who has
+        contributed most to Moot Spirit.
+      </p>
+    </div>
+    <div class="small-12 medium-6 large-4 cell">
+      <img src="/trophies/trophy-eastman.png" alt="Eastman Shield"/>
+    </div>
+
+    <div class="small-12 medium-6 large-8 cell">
+      <h3>Top Hat &amp; Bowler Trophies (Tug-O-War)</h3>
+      <p>
+        These are awarded to the male and female winners of the Tug-O-War. A
+        mixed trophy is also now up for grabs.
+      </p>
+    </div>
+    <div class="small-12 medium-6 large-4 cell">
+      <img src="/trophies/trophy-tugowar.png" alt="Top Hat &amp; Bowler Trophies (Tug-O-War)"/>
+    </div>
+
+    <div class="small-12 medium-6 large-8 cell">
+      <h3>Cross Country</h3>
+      <p>
+        Trophies are awarded to both the first individual male and female home,
+        as well as the first male and first female Crew teams home.
+      </p>
+    </div>
+    <div class="small-12 medium-6 large-4 cell">
+      <img src="/trophies/trophy-crosscountry.png" alt="Cross Country"/>
+    </div>
+
+    <div class="small-12 medium-6 large-8 cell">
+      <h3>Shooting Trophy</h3>
+      <p>
+        Awarded to the winning team in competition requiring some form of
+        marksmanship skills. Usually a round robin of party games and childhood
+        favourite games.
+      </p>
+    </div>
+    <div class="small-12 medium-6 large-4 cell">
+      <img src="/trophies/trophy-shooting.png" alt="Shooting Trophy"/>
+    </div>
+  </div>
+  <div class="fb-share-button" data-href="http://euromoot.com<?=$prefix?>/trophies" data-layout="button"></div>
 </div>
 <?php include '_below.php'; ?>
